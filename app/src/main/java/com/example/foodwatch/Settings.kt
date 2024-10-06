c72415d40686b7f71c02a2e77eeb1ef4fe463659
Catastrophy <94645810+CatastrophicCat123@users.noreply.github.com>
2024-10-05 23:40:16 -0500

    Added main page with 3 buttons
    Buttons
    - Add Reaction
    - Add Meal
    - Find Ingredients
    - Fixed minor issues

--- app/src/main/java/com/example/foodwatch/Settings.kt
+++ app/src/main/java/com/example/foodwatch/Settings.kt
@@ -1,26 +1,16 @@
 package com.example.foodwatch
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.Button;
 
-class MainActivity : AppCompatActivity() {
+class Settings : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
-//        val calButton = findViewById<Button>(R.id.calendarButton)
-//
-//        calButton.setOnClickListener {
-//            val screen = Intent(this, Calendar::class.java)
-//            startActivity(screen)
-//        }
-
+        setContentView(R.layout.activity_settings)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
