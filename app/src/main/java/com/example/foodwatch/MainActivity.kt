7a77ad07ad33dc5618d118adff2f360669849d44
JoshuaCramer <joshuacramer@my.unt.edu>
2024-10-09 16:11:44 -0500

    Added navbar functionality, refactored code to use Navigation library

--- app/src/main/java/com/example/foodwatch/MainActivity.kt
+++ app/src/main/java/com/example/foodwatch/MainActivity.kt
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import android.widget.Button;
+import androidx.fragment.app.FragmentContainerView
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
@@ -23,19 +26,25 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        /*
-        Example of how to create a button and have it change activities(pages)
-        */
+        //find navController, used to navigate to different fragments
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navFragment) as NavHostFragment
+        val navController = navHostFragment.navController
 
-        // Assign calendarButton value to be listened for
-        val navBar = findViewById<BottomNavigationView>(R.id.navBar)
+        //find bottom navigation bar
+        val navBar = findViewById<BottomNavigationView>(R.id.navBar) as BottomNavigationView
+        navBar.selectedItemId = R.id.homeButton
 
-
-
-        // Listen for button to be clicked
-        navBar.setOnClickListener{
-            val screen = Intent(this,Calendar::class.java) // Get activity to go to
-            startActivity(screen) // Activate activity
+        //set listener for when a button is selected
+        navBar.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.accountButton -> navController.navigate(R.id.to_account)
+                R.id.calendarButton -> navController.navigate(R.id.to_calendar)
+                R.id.homeButton -> navController.navigate(R.id.to_home)
+                R.id.reportsButton -> navController.navigate(R.id.to_reports)
+                R.id.optionsButton -> navController.navigate(R.id.to_options)
+            }
+            true
         }
+
     }
 }
---
+++ app/src/main/java/com/example/foodwatch/OptionsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.foodwatch
+
+import androidx.fragment.app.Fragment
+
+class OptionsFragment : Fragment(R.layout.fragment_options) {
+
+}
