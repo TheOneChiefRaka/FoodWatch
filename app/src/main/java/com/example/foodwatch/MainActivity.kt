c4911af5e560f5ea1893f95f370e537fb71c0c24
JoshuaCramer <joshuacramer@my.unt.edu>
2024-12-01 14:55:57 -0600

    Cleaned up previous commit

--- app/src/main/java/com/example/foodwatch/MainActivity.kt
+++ app/src/main/java/com/example/foodwatch/MainActivity.kt
@@ -35,16 +35,6 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        //populate ingredients database
-        val names = arrayListOf<String>(
-            "Banana", "Beef", "Bread", "Chicken", "Dairy", "Fish", "Olive Oil", "Shellfish", "Yogurt"
-        )
-
-        //if()
-        for (ingredient in names) {
-
-        }
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
