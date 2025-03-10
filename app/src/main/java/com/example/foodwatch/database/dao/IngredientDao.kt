af8ff1b49cd6f6abe628d03facce7d93b581ec1b
Kirk M. <146654281+CP7K@users.noreply.github.com>
2025-03-10 14:18:43 -0500

    Updated database and some tables

--- app/src/main/java/com/example/foodwatch/database/dao/IngredientDao.kt
+++ app/src/main/java/com/example/foodwatch/database/dao/IngredientDao.kt
@@ -10,37 +10,23 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface IngredientDao {
-    @Query("SELECT * FROM ingredient")
+    @Query("SELECT * FROM Ingredient")
     fun getAll(): Flow<List<Ingredient>>
 
-    @Query("SELECT * FROM ingredient WHERE name IN (:ingredientNames)")
+    @Query("SELECT * FROM Ingredient WHERE name IN (:ingredientNames)")
     suspend fun findIngredientsByName(ingredientNames: List<String>): List<Ingredient>
-
-    @Query("UPDATE ingredient SET mildReactions = mildReactions + 1 WHERE id IN (:ingredientIds)" )
-    suspend fun addIngredientsReactionMild(ingredientIds: List<Int>)
-
-    @Query("UPDATE ingredient SET mediumReactions = mediumReactions + 1 WHERE id IN (:ingredientIds)" )
-    suspend fun addIngredientsReactionMedium(ingredientIds: List<Int>)
-
-    @Query("UPDATE ingredient SET severeReactions = severeReactions + 1 WHERE id IN (:ingredientIds)" )
-    suspend fun addIngredientsReactionSevere(ingredientIds: List<Int>)
-
-    @Query("SELECT * FROM Ingredient WHERE (mildReactions + mediumReactions + severeReactions) > 0")
-    suspend fun findAllPossibleAllergens(): List<Ingredient>
 
     @Query("SELECT * FROM Ingredient WHERE name = :name LIMIT 1")
     suspend fun checkForIngredient(name: String): Ingredient?
 
-    @Query("UPDATE ingredient SET timesEaten = timesEaten + 1 WHERE name = :name")
-    suspend fun incrementTimesEaten(name: String)
 
-    @Query("SELECT id FROM Ingredient WHERE name = :name LIMIT 1")
+    @Query("SELECT pk_i_id FROM Ingredient WHERE name = :name LIMIT 1")
     suspend fun getIngredientIdByName(name: String): Int?
 
     @Query("SELECT name FROM Ingredient")
     suspend fun getAllIngredientNames(): List<String>
 
-    @Query("SELECT * FROM Ingredient WHERE id = :id")
+    @Query("SELECT * FROM Ingredient WHERE pk_i_id = :id")
     suspend fun findIngredientById(id: Int): Ingredient
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
