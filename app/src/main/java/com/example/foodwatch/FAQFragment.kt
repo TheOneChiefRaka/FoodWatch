e663a4ab0aef023368bf0b6016a4567b42ad9e6a
Rivadeneyra Caden <pono9898@gmail.com>
2025-04-21 01:27:09 -0500

    Adjusted account page to be faq page

--- app/src/main/java/com/example/foodwatch/FAQFragment.kt
+++ app/src/main/java/com/example/foodwatch/FAQFragment.kt
@@ -2,5 +2,5 @@ package com.example.foodwatch
 
 import androidx.fragment.app.Fragment
 
-class AccountFragment : Fragment(R.layout.fragment_account) {
+class FAQFragment : Fragment(R.layout.fragment_faq) {
 }
