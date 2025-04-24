4d11f32451a61ca26928ed3ddc7e77c11cb28c11
TheOneChiefRaka <rjmoore9361@gmail.com>
2025-04-23 20:46:31 -0500

    Added questions to faq
    Added more questions to the faq

--- app/src/main/java/com/example/foodwatch/FAQFragment.kt
+++ app/src/main/java/com/example/foodwatch/FAQFragment.kt
@@ -14,15 +14,19 @@ class FAQFragment : Fragment(R.layout.fragment_faq) {
         val faqs = listOf(
             FAQItem(
                 question = "How do I add a meal?",
-                answer   = "In the home screen, tap on \"Add Meal\"."
+                answer   = "In the home screen, tap on \"Add Meal\" and fill out the information about the meal you had. After filling out your information tap \"Add Meal\" to save your meal."
             ),
             FAQItem(
-                question = "How do I export data?",
-                answer   = "In the Options screen, tap \"Export\" and check your Downloads folder."
+                question = "How do I edit a meal I have already submitted?",
+                answer = "From the Calendar screen, find your meal on the calendar by tapping the date of it's entry. Then tap the meal you wish to enter. After making changes to your meal tap \"Save Meal\" and your meal changes will be saved to the database."
             ),
             FAQItem(
-                question = "How are you?",
-                answer = "I'm doing good!"
+                question = "How do I delete a meal?",
+                answer = "From the Calendar screen, find your meal on the calendar by tapping the date of it's entry. Then tap on the meal you wish to delete, afterwards tap on the \"Delete\" button."
+            ),
+            FAQItem(
+                question = "How do I export my data?",
+                answer   = "In the Options screen, tap \"Export Data\". This will create a CSV file of all the ingredients you have entered but will only show ingredients if they are suspected of causing allergic reactions. You can then find this file in your device's \"Downloads\" folder."
             )
         )
 
